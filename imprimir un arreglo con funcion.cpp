883c7a0900f877748c0c9af855cec25abb1a9a9c
defli7 <136860280+defli7@users.noreply.github.com>
2023-08-04 19:25:37 -0300

    Add files via upload

---
+++ imprimir un arreglo con funcion.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void imprimir(int arr[], int tamano) {
+	printf("arreglo = { ");
+	for(int i=0; i<tamano; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("}\n");
+}
+int main(void) {
+	int arr[5] = {5, 3, 4, 6, 1};
+	imprimir(arr, 5);
+	return 0;
+}
+
+
+
