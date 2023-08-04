cc412eb8f5121ee3a46f91e693e2ec4e9cf72f97
defli7 <136860280+defli7@users.noreply.github.com>
2023-08-04 19:13:41 -0300

    Add files via upload

---
+++ promedio de 10 numeros con arreglo.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int t;
+	float muestra[10];
+	float suma=0;
+	float promedio;
+	
+	for (t=0; t<10; t++){
+		printf("Ingrese el valor para muestra [%d]\n", t);
+		scanf("%f", &muestra[t]);
+		suma=suma+muestra[t];
+	}
+	
+	promedio=suma/10;
+	printf ("el promedio es: %f", promedio);
+	
+	return 0;
+}
+
+
+
