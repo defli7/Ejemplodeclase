638abae020d7dcf5b262b845d86844f643af519f
defli7 <136860280+defli7@users.noreply.github.com>
2023-08-04 19:11:36 -0300

    Add files via upload

---
+++ como agregar valores en los arreglos.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int muestra[10],t;
+	
+	for (t=0; t<10; t++){
+		printf("Ingrese el valor para muestra [%d]\n", t);
+		scanf("%d", &muestra[t]);
+	}
+	for (t=0; t<10; t++){
+		printf("muestra [%d]=%d\n", t, muestra[t]);
+	}
+	return 0;
+}
+
+
