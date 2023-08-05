1e820e0686f7d55bdc4eb0832bfdc4979e2bd1bf
defli7 <136860280+defli7@users.noreply.github.com>
2023-08-04 21:23:40 -0300

    Add files via upload

---
+++ TP6-1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+int rec2polar(a,b,r,tita){
+	
+}
+
+int main(){
+char opcion;
+int a=0, b=0;
+printf ("Ingrese el calculo que quiera realizar: \n a)De rectangular a polar \n b)De polar a rectangular\n");
+scanf ("%c", &opcion);
+
+if (opcion=='a'){
+	printf("Ingrse los valores de a y b");
+	scanf ("%d", &a);
+	scanf ("%d", &b);
+	rec2polar(a,b,r,tita);
+}else if(opcion=='b'){
+	printf("Ingrse los valores de a y b");
+	scanf ("%d", &r);
+	scanf ("%d", &tita);
+	polar2rec(r,tita,a,b);
+}
+
